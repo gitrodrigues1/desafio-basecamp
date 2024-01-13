d72a4c2a26ab98b3c56bfc6b3158f7356bcd4969
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-01-13 01:59:57 -0300

    upd Person

--- src/main/java/br/com/marcos/desafiobasecamp/person/Person.java
+++ src/main/java/br/com/marcos/desafiobasecamp/person/Person.java
@@ -1,18 +1,23 @@
 package br.com.marcos.desafiobasecamp.person;
 
+import br.com.marcos.desafiobasecamp.todo.entity.TodoItem;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 public class Person {
 
@@ -26,4 +31,8 @@ public class Person {
     @Email
     @NotBlank
     private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "todo_id")
+    private TodoItem toDo;
 }
