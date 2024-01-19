3b5010023238f88e42f617ff2297ea68bd11d527
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-01-19 03:58:26 -0300

    Add lombok @Builder annotation to CreateProjectRequest class

--- src/main/java/br/com/marcos/desafiobasecamp/project/Project.java
+++ src/main/java/br/com/marcos/desafiobasecamp/project/Project.java
@@ -1,11 +1,9 @@
 package br.com.marcos.desafiobasecamp.project;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
